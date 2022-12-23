e6f43ffcbbf59826dfc9fba7603f90808c475f49
Jonathan Rosewood <jonathan-rosewood@yandex.ru>
2022-12-23 17:42:44 +0600

    Icon & update functional

--- script.code.js
+++ script.code.js
@@ -3,8 +3,12 @@
 // @description Adds start chat button to sidebar buttons group
 // @namespace   MeddleMonkey Scripts
 // @author      JR Agency
-// @version     0.1
+// @version     0.2
 // @homepage    https://github.com/Jonathan-Rosewood/whatsapp-start-chat-button-userscript
+// @icon64      https://github.com/Jonathan-Rosewood/whatsapp-start-chat-button-userscript/raw/main/icon-16.png
+// @icon64      https://github.com/Jonathan-Rosewood/whatsapp-start-chat-button-userscript/raw/main/icon-64.png
+// @updateURL   https://github.com/Jonathan-Rosewood/whatsapp-start-chat-button-userscript/raw/main/script.meta.js
+// @downloadURL https://github.com/Jonathan-Rosewood/whatsapp-start-chat-button-userscript/raw/main/script.code.js
 // @include     http://web.whatsapp.com/*
 // @include     https://web.whatsapp.com/*
 // @run-at      document-end
